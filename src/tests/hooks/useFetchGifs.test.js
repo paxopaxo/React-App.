74cf4b8756b51699de5630c6589290bf7dafef2a
Max <paxipaxi2003@gmail.com>
2021-07-27 19:04:15 -0400

    2707

--- src/tests/hooks/useFetchGifs.test.js
+++ src/tests/hooks/useFetchGifs.test.js
@@ -3,20 +3,29 @@ import { renderHook } from '@testing-library/react-hooks'
 
 describe('Pruebas con el Hook useFetchGifs', () => {
     
-    test('debe de retornar el estado inicial', () => {
+    test('debe de retornar el estado inicial', async() => {
         
-        const { result } = renderHook( () => useFetchGifs( 'One Punch' ) )
-        
+        const { result, waitForNextUpdate } = renderHook( () => useFetchGifs( 'One Punch' ) )
         const { data, loading } = result.current 
 
-        // const { data, loading } = useFetchGifs( 'One Punch' ) 
-
-        // console.log( data ) 
-        // console.log( loading ) 
+        // await waitForNextUpdate()
 
         expect( data ).toEqual( [] )
         expect( loading ).toBe( true )
     })
     
+    test('debe de retornar un arreglo de imgs y el loading en false', async() => {
+        
+        const { result, waitForNextUpdate } = renderHook( () => useFetchGifs( 'One Punch' ) )
+        
+        await waitForNextUpdate()
+        
+        const { data, loading } = result.current 
+        
+        expect( data.length ).toBe( 10 )
+        expect( loading ).toBe( false )
+
+    })
+
 
 })
